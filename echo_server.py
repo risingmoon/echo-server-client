69ce4f7aedb724b2e48bbca74713c4f3dbdead93
risingmoon <justindavidlee88@gmail.com>
2014-02-18 18:23:19 -0800

    Modified echo_server.py and echo_client.py

--- echo_server.py
+++ echo_server.py
@@ -1,25 +1,41 @@
 import socket
 
-buffsize = 256
 
-if __name__ == "__main__":
-	server_socket = socket.socket(
+
+def socket_setup():
+	"""Returns and setup a socket based on this
+	project's configuration"""
+	return socket.socket(
 		socket.AF_INET,
 		socket.SOCK_STREAM,
 		socket.IPPROTO_TCP)
 
+def recv(connection, buffer_size):
+
+	response = ''
+	done = False
+	while not done:
+		msg_part = connection.recv(buffer_size)
+		response += msg_part
+		if len(msg_part) < buffer_size:
+			done = True
+	return response
+
+if __name__ == "__main__":
+	#Setup server socket with socket_setup()
+	server_socket = socket_setup()
+
+	#Bind and set server socket to listen
 	server_socket.bind(('127.0.0.1',50000))
 	server_socket.listen(1)
 
-	conn, addr = server_socket.accept()
-	done = False
-	response = ''
+	connection, address = server_socket.accept()
 
-	while not done:
-		msg_part = conn.recv(buffsize)
-		print msg_part
-		if len(msg_part) < buffsize:
-			done = True
-			conn.sendall("message received")
-			conn.shutdown(socket.SHUT_WR)
-			conn.close()
+	response = recv(connection,8)
+
+	connection.sendall("message received")
+	connection.shutdown(socket.SHUT_WR)
+	connection.close()
+
+	print response
+
